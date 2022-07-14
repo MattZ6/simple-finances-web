50e56e35a785b521ee0a177d7c33887ef271d329
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-07-14 16:52:26 -0300

    chore(TransactionCategoryIcon): add some icons

--- src/components/TransactionCategoryIcon/index.tsx
+++ src/components/TransactionCategoryIcon/index.tsx
@@ -13,15 +13,20 @@ import {
   Scroll,
   ShoppingCart,
   TShirt,
+  Wallet,
+  CurrencyDollar,
+  CurrencyDollarSimple,
+  Note,
 } from 'phosphor-react';
 import { memo } from 'react';
 
 type Props = {
   label: string;
+  type: 'INCOME' | 'OUTCOME';
   size: number;
 };
 
-export const TransactionCategoryIcon = memo(({ label, size }: Props) => {
+export const TransactionCategoryIcon = memo(({ label, type, size }: Props) => {
   if (label === 'feed') {
     return <ForkKnife size={size} />;
   }
@@ -72,6 +77,22 @@ export const TransactionCategoryIcon = memo(({ label, size }: Props) => {
 
   if (label === 'taxes') {
     return <Scroll size={size} />;
+  }
+
+  if (label === 'salary') {
+    return <CurrencyDollar size={size} />;
+  }
+
+  if (label === 'service') {
+    return <Note size={24} />;
+  }
+
+  if (label === 'loan') {
+    return <Wallet size={24} />;
+  }
+
+  if (type === 'INCOME') {
+    return <CurrencyDollarSimple size={size} />;
   }
 
   return <Receipt size={size} />;
